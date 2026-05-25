8c338495d654cc8bda85e1499bafea8e02d27ec0
Bruno0416 <brunomcalderonv@gmail.com>
2026-05-25 14:13:37 -0400

    fix entidad

--- src/main/java/com/mariluz/auth_service/model/User.java
+++ src/main/java/com/mariluz/auth_service/model/User.java
@@ -15,10 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder
-@ToString(exclude = "password") // password (hash) excluido para no exponerlo en logs
-@EqualsAndHashCode(of = "id") // equals/hashCode solo por id, evita conflictos con colecciones Hibernate
 @Entity(name = "users")
 public class User implements UserDetails {
 
