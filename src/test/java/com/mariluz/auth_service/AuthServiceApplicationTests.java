55aab4c7f4f842598cfc2e310d66edb2f033b4f3
Bruno0416 <brunomcalderonv@gmail.com>
2026-06-19 14:09:19 -0400

    Update AuthServiceApplicationTests.java

--- src/test/java/com/mariluz/auth_service/AuthServiceApplicationTests.java
+++ src/test/java/com/mariluz/auth_service/AuthServiceApplicationTests.java
@@ -1,6 +1,11 @@
 package com.mariluz.auth_service;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthServiceApplicationTests {}
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
